ced581c31410c3b360faf616dd4e9e06ab94124e
Aron Woost <aron@springworks.de>
2024-02-12 11:14:43 +0100

    Provide hardcoded csv list

--- connect.js
+++ connect.js
@@ -1,40 +1,11 @@
+import fs from 'fs';
 import { ChatOpenAI } from '@langchain/openai';
 import { ChatPromptTemplate } from '@langchain/core/prompts';
 import { StringOutputParser } from '@langchain/core/output_parsers';
-import { CheerioWebBaseLoader } from 'langchain/document_loaders/web/cheerio';
-import { convert } from './convertHtmlTabelToCsv.js';
+
+const csv = fs.readFileSync('./finals.csv', { encoding: 'utf-8' });
 
 export const connect = async ({ input, verbose = false }) => {
-  const loader = new CheerioWebBaseLoader(
-    'https://en.wikipedia.org/wiki/List_of_European_Cup_and_UEFA_Champions_League_finals',
-    {
-      timeout: 2000,
-    }
-  );
-
-  const $ = await loader.scrape();
-
-  $('a').contents().unwrap();
-  $('.flagicon').remove();
-  $('sup').remove();
-  $('[style]').removeAttr('style');
-  $('[class]').removeAttr('class');
-  $('[title]').removeAttr('title');
-  $('[scope]').removeAttr('scope');
-  $('[align]').removeAttr('align');
-  $('[colspan]').removeAttr('colspan');
-  $('[rowspan]').removeAttr('rowspan');
-  $('[bgcolor]').removeAttr('bgcolor');
-  $('[data-sort-value]').removeAttr('data-sort-value');
-  $('span').contents().unwrap();
-
-  const text = $('table')
-    .eq(2)
-    .html()
-    .replace(/(\r\n|\n|\r)/gm, '');
-
-  const csv = convert({ input: `<table>${text}</table>` });
-
   const chatModel = new ChatOpenAI({
     // modelName: 'gpt-4',
     modelName: 'gpt-3.5-turbo',
